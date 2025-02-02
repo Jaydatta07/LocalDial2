efa80f9a72d4b01922dbdb050874a995cbe3fc9e
jaydatta shinde <jaydattashinde2004@gmail.com>
2025-02-02 14:31:33 +0530

    cheb qwfd8owqsh

--- frontend/src/endPoint.js
+++ frontend/src/endPoint.js
@@ -1,6 +1,7 @@
 // src/api/endPoint.js
+export const BACKEND_URL = `https://localdial2-4.onrender.com`
 
-export const API_BASE_URL = `https://localdial2-4.onrender.com/api/user`; // Your backend URL
+export const API_BASE_URL = `${BACKEND_URL}/api/user`; // Your backend URL
 
 // Registration Endpoint
 export const REGISTER_URL = `${API_BASE_URL}/register`;
@@ -12,8 +13,8 @@ export const LOGIN_URL = `${API_BASE_URL}/login`;
 export const RESET_PASSWORD_URL = `${API_BASE_URL}/resetpassword`;
 
 
-export const API_PROUDCT_URL = `https://localdial2-4.onrender.com/api/businesses`;
+export const API_PROUDCT_URL = `${BACKEND_URL}/api/businesses`;
 
-export const API_IMAGE_UPLOAD_URL =`https://localdial2-4.onrender.com/api/img/upload`;
+export const API_IMAGE_UPLOAD_URL =`${BACKEND_URL}/api/img/upload`;
 
-export const API_URL =`https://localdial2-4.onrender.com/api/login`
+export const API_URL =`${BACKEND_URL}/api/login`
